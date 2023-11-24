55d4d9a61970c068a2208aa66bb2e50bd4e3daa0
Nico <nicos.t.65@gmail.com>
2023-11-25 00:31:56 +0700

    feat: make base model

--- app/src/main/java/proyek/andro/MainActivity.kt
+++ app/src/main/java/proyek/andro/MainActivity.kt
@@ -1,11 +1,24 @@
 package proyek.andro
 
+import TournamentSeeder
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import proyek.andro.seeder.GameSeeder
+
 
 class MainActivity : AppCompatActivity() {
+
+    var runSeeder = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (runSeeder) {
+            GameSeeder().run()
+            TournamentSeeder().run()
+        }
+
+        startActivity(Intent(this, Homepage::class.java))
     }
 }
