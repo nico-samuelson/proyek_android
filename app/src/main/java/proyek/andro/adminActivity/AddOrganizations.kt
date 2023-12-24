761b56091d4adc4b76fbb0ba49272a26ff21cd8f
Kelvin Sidharta Sie <118882421+nivleking@users.noreply.github.com>
2023-12-24 20:45:56 +0700

    feat: crud org + team almost done

--- app/src/main/java/proyek/andro/adminActivity/AddOrganizations.kt
+++ app/src/main/java/proyek/andro/adminActivity/AddOrganizations.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.AutoCompleteTextView
 import android.widget.ImageView
 import android.widget.TextView
@@ -11,6 +12,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.google.firebase.firestore.Filter
 import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
@@ -18,20 +20,25 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.w3c.dom.Text
 import proyek.andro.R
+import proyek.andro.helper.StorageHelper
 import proyek.andro.model.Organization
 import java.util.UUID
 
 class AddOrganizations : AppCompatActivity() {
-    private lateinit var inputLogo : ImageView
-    private lateinit var etName : TextView
-    private lateinit var etCEO : TextView
-    private lateinit var etFounded : TextView
-    private lateinit var etLocation : TextView
-    private lateinit var etWebsite : TextView
-    private lateinit var etDescription : TextView
-    private lateinit var etStatus : AutoCompleteTextView
+    private lateinit var inputLogo: ImageView
+    private lateinit var etName: TextView
+    private lateinit var etCEO: TextView
+    private lateinit var etFounded: TextView
+    private lateinit var etLocation: TextView
+    private lateinit var etWebsite: TextView
+    private lateinit var etDescription: TextView
+    private lateinit var etStatus: AutoCompleteTextView
 
-    private var imageURI : Uri? = null
+    private var mode: String? = null
+    private var name: String? = null
+    private var organization: Organization? = null
+
+    private var imageURI: Uri? = null
 
     private val storageRef = FirebaseStorage.getInstance().reference
 
@@ -48,10 +55,21 @@ class AddOrganizations : AppCompatActivity() {
         etDescription = findViewById(R.id.etDescription)
         etStatus = findViewById(R.id.etStatus)
 
-        val backBtn : ImageView = findViewById(R.id.backBtn)
-        val submitBtn : MaterialButton = findViewById(R.id.submitBtn)
+        val backBtn: ImageView = findViewById(R.id.backBtn)
+        val submitBtn: MaterialButton = findViewById(R.id.submitBtn)
+        val pageTitle: TextView = findViewById(R.id.pageTitle)
 
         (etStatus as MaterialAutoCompleteTextView).setSimpleItems(arrayOf("Active", "Inactive"))
+
+        mode = intent.getStringExtra("mode")
+        name = intent.getStringExtra("name")
+
+        if (mode == "edit") {
+            pageTitle.text = "Edit Organization"
+            submitBtn.text = "Save"
+
+            showEdit()
+        }
 
         backBtn.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
@@ -63,8 +81,7 @@ class AddOrganizations : AppCompatActivity() {
                     setBackgroundTint(resources.getColor(R.color.light, null))
                     setTextColor(resources.getColor(R.color.black, null))
                 }.show()
-            }
-            else {
+            } else {
                 val name = etName.text.toString()
                 val ceo = etCEO.text.toString()
                 val founded = etFounded.text.toString()
@@ -73,16 +90,36 @@ class AddOrganizations : AppCompatActivity() {
                 val description = etDescription.text.toString()
                 val status = etStatus.text.toString()
 
-                var logo = ""
-                if (imageURI != null) {
-                    CoroutineScope(Dispatchers.Main).launch {
-                        logo = uploadPhoto(imageURI!!)
-                    }.invokeOnCompletion {
+                if (mode == "add") {
+                    if (imageURI != null) {
+                        CoroutineScope(Dispatchers.Main).launch {
+                            val logo = uploadPhoto(imageURI!!)
+                            val newOrg = Organization(
+                                UUID.randomUUID().toString(),
+                                name,
+                                description,
+                                logo,
+                                founded,
+                                location,
+                                website,
+                                ceo,
+                                if (status == "Active") 1 else 0,
+                            )
+
+                            newOrg.insertOrUpdate()
+                            startActivity(
+                                Intent(
+                                    this@AddOrganizations,
+                                    ManageOrganizations::class.java
+                                )
+                            )
+                        }
+                    } else {
                         val newOrg = Organization(
                             UUID.randomUUID().toString(),
                             name,
                             description,
-                            logo,
+                            "",
                             founded,
                             location,
                             website,
@@ -92,23 +129,79 @@ class AddOrganizations : AppCompatActivity() {
 
                         CoroutineScope(Dispatchers.Main).launch {
                             newOrg.insertOrUpdate()
-                            startActivity(Intent(this@AddOrganizations, ManageOrganizations::class.java))
+                            startActivity(
+                                Intent(
+                                    this@AddOrganizations,
+                                    ManageOrganizations::class.java
+                                )
+                            )
                         }
+                    }
+                } else if (mode == "edit") {
+                    if (imageURI != null) {
+                        CoroutineScope(Dispatchers.Main).launch {
+                            StorageHelper().deleteFile(organization?.logo ?: "")
+                            organization?.logo = "/logo/orgs/${uploadPhoto(imageURI!!)}"
+                        }
+                    }
+                    organization?.name = name
+                    organization?.ceo = ceo
+                    organization?.founded = founded
+                    organization?.location = location
+                    organization?.website = website
+                    organization?.description = description
+                    organization?.status = if (status == "Active") 1 else 0
+
+                    CoroutineScope(Dispatchers.Main).launch {
+                        organization?.insertOrUpdate()
+                        startActivity(
+                            Intent(
+                                this@AddOrganizations,
+                                ManageOrganizations::class.java
+                            )
+                        )
                     }
                 }
             }
         }
 
         // file upload listener
-        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
-            Picasso.get().load(uri).into(inputLogo)
-            imageURI = uri
-        }
+        val getContent =
+            registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+                Picasso.get().load(uri).into(inputLogo)
+                imageURI = uri
+            }
 
         inputLogo.setOnClickListener { getContent.launch("image/*") }
     }
 
-    fun uploadPhoto(uri : Uri) : String{
+    fun showEdit() {
+        CoroutineScope(Dispatchers.Main).launch {
+            organization = Organization().get<Organization>(
+                filter = Filter.equalTo("name", name),
+                limit = 1,
+                order = arrayOf(arrayOf("name", "asc"))
+            ).firstOrNull()
+
+            if (organization != null) {
+                etName.text = organization?.name
+                etCEO.text = organization?.ceo
+                etFounded.text = organization?.founded
+                etLocation.text = organization?.location
+                etWebsite.text = organization?.website
+                etDescription.text = organization?.description
+                etStatus.setText(if (organization?.status == 1L) "Active" else "Inactive")
+
+                val formattedLogo = "logo_" + organization?.name?.replace(" ","_")?.toLowerCase() + ".png"
+                storageRef.child("logo/orgs/${formattedLogo}").downloadUrl.addOnSuccessListener {
+                    Log.d("editOrg", it.toString())
+                    Picasso.get().load(it).into(inputLogo)
+                }
+            }
+        }
+    }
+
+    fun uploadPhoto(uri: Uri): String {
         val photoID = UUID.randomUUID().toString()
         val fileRef = storageRef.child("logo/orgs/${etName.text.toString()}")
 
@@ -132,7 +225,7 @@ class AddOrganizations : AppCompatActivity() {
         return photoID
     }
 
-    fun validate() : Boolean {
+    fun validate(): Boolean {
         if (etName.text.toString().isEmpty() || etCEO.text.toString().isEmpty() ||
             etFounded.text.toString().isEmpty() || etLocation.text.toString().isEmpty() ||
             etWebsite.text.toString().isEmpty() || etDescription.text.toString().isEmpty()
--- app/src/main/java/proyek/andro/adminActivity/AddTeams.kt
+++ app/src/main/java/proyek/andro/adminActivity/AddTeams.kt
@@ -11,6 +11,7 @@ import android.widget.TextView
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.google.firebase.firestore.Filter
 import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
@@ -24,17 +25,20 @@ import java.util.Locale
 import java.util.UUID
 
 class AddTeams : AppCompatActivity() {
-    private var games : ArrayList<Game> = ArrayList()
-    private var organizations : ArrayList<Organization> = ArrayList()
-    private var team : Team? = null
+    private var games: ArrayList<Game> = ArrayList()
+    private var organizations: ArrayList<Organization> = ArrayList()
+    private var team: Team? = null
 
-    private lateinit var etName : TextView
-    private lateinit var etGame : AutoCompleteTextView
-    private lateinit var etFounded : TextView
-    private lateinit var etCoach : TextView
-    private lateinit var etStatus : AutoCompleteTextView
-    private lateinit var etOrg : AutoCompleteTextView
-    private lateinit var etDescription : TextView
+    private var mode: String? = null
+    private var name: String? = null
+
+    private lateinit var etName: TextView
+    private lateinit var etGame: AutoCompleteTextView
+    private lateinit var etFounded: TextView
+    private lateinit var etCoach: TextView
+    private lateinit var etStatus: AutoCompleteTextView
+    private lateinit var etOrg: AutoCompleteTextView
+    private lateinit var etDescription: TextView
 
     private val storageRef = FirebaseStorage.getInstance().reference
 
@@ -50,10 +54,12 @@ class AddTeams : AppCompatActivity() {
         etOrg = findViewById(R.id.etOrg)
         etDescription = findViewById(R.id.etDescription)
 
-        val backBtn : ImageView = findViewById(R.id.backBtn)
-        val submitBtn : MaterialButton = findViewById(R.id.submitBtn)
+        val backBtn: ImageView = findViewById(R.id.backBtn)
+        val submitBtn: MaterialButton = findViewById(R.id.submitBtn)
+        val pageTitle: TextView = findViewById(R.id.pageTitle)
 
-        (etStatus as MaterialAutoCompleteTextView).setSimpleItems(arrayOf("Active", "Inactive"))
+        mode = intent.getStringExtra("mode")
+        name = intent.getStringExtra("name")
 
         backBtn.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
@@ -69,6 +75,13 @@ class AddTeams : AppCompatActivity() {
             (etOrg as MaterialAutoCompleteTextView).setSimpleItems(
                 organizations.map { it.name }.toTypedArray()
             )
+            (etStatus as MaterialAutoCompleteTextView).setSimpleItems(arrayOf("Active", "Inactive"))
+            if (mode == "edit") {
+                pageTitle.text = "Edit Team"
+                submitBtn.text = "Save"
+
+                showEdit()
+            }
         }
 
         submitBtn.setOnClickListener {
@@ -77,8 +90,7 @@ class AddTeams : AppCompatActivity() {
                     setBackgroundTint(resources.getColor(R.color.light, null))
                     setTextColor(resources.getColor(R.color.black, null))
                 }.show()
-            }
-            else {
+            } else {
                 val name = etName.text.toString()
                 val game = etGame.text.toString()
                 val founded = etFounded.text.toString()
@@ -89,35 +101,88 @@ class AddTeams : AppCompatActivity() {
 
                 val orgLogo = organizations.filter { it.name == org }.map { it.logo }.firstOrNull()
                 if (orgLogo == null) {
-                    Log.d("AddTeams", "Organization logo not found")
+                    Log.d("AddTeams", "Team logo not found")
                     return@setOnClickListener
                 }
-                Log.d("AddTeams", "Organization logo found: $orgLogo")
+                Log.d("AddTeams", "Team logo found: $orgLogo")
 
-                val newTeam = Team(
-                    UUID.randomUUID().toString(),
-                    game,
-                    org,
-                    name,
-                    orgLogo,
-                    coach,
-                    founded,
-                    description,
-                    if (status == "Active") 1 else 0,
+                if (mode == "add") {
+                    Log.d("addteams",games.find { it.name == game }?.id!!.toString())
+                    Log.d("addteams",organizations.find { it.name == org }?.id!!.toString())
+                    val newTeam = Team(
+                        UUID.randomUUID().toString(),
+                        organizations.find { it.name == org }?.id!!,
+                        games.find { it.name == game }?.id!!,
+                        name,
+                        orgLogo,
+                        coach,
+                        founded,
+                        description,
+                        if (status == "Active") 1 else 0,
+                    )
+
+                    CoroutineScope(Dispatchers.Main).launch {
+                        newTeam.insertOrUpdate()
+                        startActivity(Intent(this@AddTeams, ManageTeams::class.java))
+                    }
+                } else if (mode == "edit") {
+                    team?.organization = organizations.find { it.name == org }?.id!!
+                    team?.game = games.find { it.name == game }?.id!!
+                    team?.name = name
+                    team?.logo = orgLogo
+                    team?.coach = coach
+                    team?.founded = founded
+                    team?.description = description
+                    team?.status = if (status == "Active") 1 else 0
+
+                    CoroutineScope(Dispatchers.Main).launch {
+                        team?.insertOrUpdate()
+                        startActivity(Intent(this@AddTeams, ManageTeams::class.java))
+                    }
+                }
+            }
+        }
+
+    }
+
+    fun showEdit() {
+        CoroutineScope(Dispatchers.Main).launch {
+            team = Team().get<Team>(
+                filter = Filter.equalTo("name", name!!),
+                limit = 1,
+                order = arrayOf(arrayOf("name", "asc"))
+            ).firstOrNull()
+            if (team != null) {
+                Log.d("AddTeams", team.toString())
+//            Log.d("AddTeams", "Team found: $team")
+
+                etName.text = team!!.name
+                etGame.setText(games.find { it.id == team?.game }?.name)
+                Log.d("showedit", games.find { it.id == team?.game }?.name.toString())
+                etFounded.text = team!!.founded
+                etCoach.text = team!!.coach
+                etStatus.setText(if (team!!.status == 1L) "Active" else "Inactive", false)
+                etOrg.setText(organizations.find { it.id == team?.organization }?.name)
+                etDescription.text = team!!.description
+
+                (etGame as MaterialAutoCompleteTextView).setSimpleItems(
+                    games.map { it.name }.toTypedArray()
                 )
 
-                CoroutineScope(Dispatchers.Main).launch {
-                    newTeam.insertOrUpdate()
-                    startActivity(Intent(this@AddTeams, ManageOrganizations::class.java))
-                }
+                (etOrg as MaterialAutoCompleteTextView).setSimpleItems(
+                    organizations.map { it.name }.toTypedArray()
+                )
+
+                (etStatus as MaterialAutoCompleteTextView).setSimpleItems(arrayOf("Active", "Inactive"))
             }
         }
     }
 
-    fun validate() : Boolean {
+    fun validate(): Boolean {
         if (etName.text.toString().isEmpty() || etGame.text.toString().isEmpty() ||
             etFounded.text.toString().isEmpty() || etCoach.text.toString().isEmpty() ||
-            etStatus.text.toString().isEmpty() || etOrg.text.toString().isEmpty() || etDescription.text.toString().isEmpty()
+            etStatus.text.toString().isEmpty() || etOrg.text.toString()
+                .isEmpty() || etDescription.text.toString().isEmpty()
         ) {
             return false
         }
